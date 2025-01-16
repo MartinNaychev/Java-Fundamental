92c9e9c129f7d9a6c8d7ac3bec8675a9f56e207a
Martin Naychev <martin.naychev@gmail.com>
2025-01-16 20:12:24 +0200

    Create ForeignLanguages.java

---
+++ BasicSyntaxConditionalStatementsAndLoops/ForeignLanguages.java
@@ -0,0 +1,21 @@
+package BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String country = scanner.nextLine();
+
+        if ("USA".equals(country) || "England".equals(country)) {
+            System.out.println("English");
+
+        } else if ("Spain".equals(country) || "Argentina".equals(country) || "Mexico".equals(country)) {
+            System.out.println("Spanish");
+        }else {
+            System.out.println("unknown");
+        }
+
+
+    }
+}
