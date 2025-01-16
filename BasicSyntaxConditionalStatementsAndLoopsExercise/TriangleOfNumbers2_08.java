037177bf1cc111900e54a03200aff8a8d4d5c449
Martin Naychev <martin.naychev@gmail.com>
2025-01-16 20:16:58 +0200

    Create TriangleOfNumbers2_08.java

---
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/TriangleOfNumbers2_08.java
@@ -0,0 +1,19 @@
+package BasicSyntaxConditionalStatementsAndLoopsExercise;
+
+import java.util.Scanner;
+
+public class TriangleOfNumbers2_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int row = 1; row <= number; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(row + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
