98f689cea096223b192f193d61a1b9b88f3a3668
Martin Naychev <martin.naychev@gmail.com>
2025-02-04 14:41:21 +0200

    Update MathPower.java

--- Methods/MathPower.java
+++ Methods/MathPower.java
@@ -5,7 +5,16 @@ import java.util.Scanner;
 public class MathPower {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int power = Integer.parseInt(scanner.nextLine());
 
+        double poweredNumber = validateNumber(number,power);
+        System.out.printf("%.0f",poweredNumber);
 
+    }
+
+    private static double validateNumber (int number, int power){
+
+        return Math.pow(number,power);
     }
 }
