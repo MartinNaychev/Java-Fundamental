7aaf0bb34dbb40a438ac0224d86a665e689e0485
Martin Naychev <martin.naychev@gmail.com>
2025-02-09 19:51:50 +0200

    Create ListManipulationAdvanced.java

---
+++ List/ListManipulationAdvanced.java
@@ -0,0 +1,9 @@
+package List;
+
+import java.util.Scanner;
+
+public class ListManipulationAdvanced {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
