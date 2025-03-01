203c05a1e09184e9cf7598f3a51610793701ebc1
Martin Naychev <martin.naychev@gmail.com>
2025-03-01 14:23:19 +0200

    Update Main.java

--- ObjectsAndClasses/Student2/Main.java
+++ ObjectsAndClasses/Student2/Main.java
@@ -21,18 +21,19 @@ public class Main {
             int age = Integer.parseInt(input.split(" ")[2]);
             String hometown = input.split(" ")[3];
 
-            Student student = new Student(firstName, lastName, age, hometown);
-            for (Student studentForCheck : studentList) {
-                if (!studentForCheck.getFirstName().equals(firstName)){
-                    if (!studentForCheck.getLastName().equals(lastName)){
-                        studentList.add(student);
-                    }
-                }
 
-            }
-            if (studentList.isEmpty()){
+
+            if (ifStudentExist(studentList,firstName,lastName)){
+                Student student = getStudent(studentList,firstName,lastName);
+                student.setFirstName(firstName);
+                student.setLastName(lastName);
+                student.setAge(age);
+                student.setHometown(hometown);
+            }else {
+                Student student = new Student(firstName, lastName, age, hometown);
                 studentList.add(student);
             }
+
 
 
 
@@ -51,4 +52,38 @@ public class Main {
 
     }
 
+    private static boolean ifStudentExist (List<Student> studentList, String firstName, String lastName){
+        for (Student student : studentList) {
+            if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private static Student getStudent (List<Student> studentList, String firstname, String lastName){
+        Student existingStudent = null;
+
+        for (Student student : studentList) {
+            if (student.getFirstName().equals(firstname) && student.getLastName().equals(lastName)){
+                existingStudent = student;
+            }
+        }
+        return existingStudent;
+    }
+
 }
+
+
+//for (Student studentForCheck : studentList) {
+//                if (!studentForCheck.getFirstName().equals(firstName)){
+//                    if (!studentForCheck.getLastName().equals(lastName)){
+//                        studentList.add(student);
+//                    }
+//                }
+//
+//            }
+//            if (studentList.isEmpty()){
+//                studentList.add(student);
+//            }
