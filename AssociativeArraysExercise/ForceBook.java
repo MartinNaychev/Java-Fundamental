5877182338f6508d807cfa86fea8f12895e5d157
Martin Naychev <martin.naychev@gmail.com>
2025-03-05 21:09:24 +0200

    Create ForceBook.java

---
+++ AssociativeArraysExercise/ForceBook.java
@@ -0,0 +1,11 @@
+package AssociativeArraysExercise;
+
+import java.util.Scanner;
+
+public class ForceBook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
