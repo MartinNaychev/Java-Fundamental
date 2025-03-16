62a863ad1320abd6f108eb656d0e13d31e39924c
Martin Naychev <martin.naychev@gmail.com>
2025-03-16 18:03:03 +0200

    Create StarEnigma.java

---
+++ RegularExpressionsExercise/StarEnigma.java
@@ -0,0 +1,11 @@
+package RegularExpressionsExercise;
+
+import java.util.Scanner;
+
+public class StarEnigma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
