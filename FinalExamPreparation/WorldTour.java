0bc7f0118046a3ae65dbbeca7df82c162b919842
Martin Naychev <martin.naychev@gmail.com>
2025-03-26 12:16:08 +0200

    Create WorldTour.java

---
+++ FinalExamPreparation/WorldTour.java
@@ -0,0 +1,38 @@
+package FinalExamPreparation;
+
+import java.util.Scanner;
+
+public class WorldTour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+     //  String destinations = scanner.nextLine();
+     //  String command = scanner.nextLine();
+
+     //  while (!command.equals("Travel")) {
+     //      String operation = command.split(":")[0];
+
+     //      if (operation.contains("Add")) {
+     //          int index = Integer.parseInt(command.split(":")[1]);
+     //          String stringForInsert = command.split(":")[2];
+
+
+     //          if (index >= 0 && index < destinations.length()) {
+     //              StringBuilder newString = new StringBuilder(destinations);
+     //              newString.insert(index, stringForInsert);
+     //              destinations = newString.toString();
+     //          }
+
+
+
+     //      } else if (operation.contains("Remove")) {
+
+     //      } else if (operation.contains("Swith")) {
+
+     //      }
+
+     //      System.out.println();
+     //      command = scanner.nextLine();
+     //  }
+    }
+}
