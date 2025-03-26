882064892863b1ac73ea63b4d2bc4315abf25361
Martin Naychev <martin.naychev@gmail.com>
2025-03-26 11:39:16 +0200

    Update PasswordReset.java

--- FinalExamPreparation/PasswordReset.java
+++ FinalExamPreparation/PasswordReset.java
@@ -2,7 +2,7 @@ package FinalExamPreparation;
 
 import java.util.Scanner;
 
-public class PasswordReset {
+public class   PasswordReset {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
